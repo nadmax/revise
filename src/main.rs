7a08859daf17c6f19470f1b01d45529d7855292a
nadmax <n.maximilien1@gmail.com>
2023-08-05 20:45:52 +0200

    add proper terminal

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
+mod terminal;
 
 use editor::Editor;
+pub use terminal::Terminal;
 
 fn main() {
-    Editor::default().run();
+    Editor::new().run();
 }
