9016f9adc4147dd4b60a96918c8873dd2306d9fc
nadmax <n.maximilien1@gmail.com>
2023-08-05 18:58:36 +0200

    add screen clearing

--- src/editor.rs
+++ src/editor.rs
@@ -1,37 +1,67 @@
-use std::io::{self, stdout, Error};
+use std::io::{stdin, stdout, Write, Error};
 use termion::raw::IntoRawMode;
 use termion::event::Key;
 use termion::input::TermRead;
+use termion::{clear, cursor};
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{key:?}\r"),
-                },
-                Err(err) => die(&err),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(&error);
+            }
+
+            if self.should_quit {
+                break;
+            }
+
+            if let Err(error) = self.refresh_screen() {
+                die(&error);
             }
         }
     }
 
     pub fn default() -> Self {
-        Self {}
+        Self {should_quit: false}
+    }
+
+    fn process_keypress(&mut self) -> Result<(), Error> {
+        let pressed_key = read_key()?;
+
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (),
+        }
+
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), Error> {
+        print!("{}{}", clear::All, cursor::Goto(1, 1));
+
+        if self.should_quit {
+            println!("Goodbye!\r");
+        }
+
+        stdout().flush()
     }
 }
 
 fn die(err: &Error) {
+    print!("{}", clear::All);
     panic!("{}", err)
+}
+
+fn read_key() -> Result<Key, Error> {
+    loop {
+        if let Some(key) = stdin().lock().keys().next() {
+            return key;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,5 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
-
-    editor.run();
+    Editor::default().run();
 }
