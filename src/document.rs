c4d30303d8643d84e6b202bbf29e12edd0d07e43
nadmax <n.maximilien1@gmail.com>
2023-08-06 17:30:25 +0200

    add characters insertion with deletion

--- src/document.rs
+++ src/document.rs
@@ -1,4 +1,5 @@
 use crate::Row;
+use crate::Position;
 
 use std::fs;
 use std::io::Error;
@@ -40,5 +41,66 @@ impl Document {
     #[must_use]
     pub fn len(&self) -> usize {
         self.rows.len()
+    }
+
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if c == '\n' {
+            self.insert_newline(at);
+
+            return;
+        }
+        
+        if at.y == self.len() {
+            let mut row = Row::default();
+
+            row.insert(0, c);
+            self.rows.push(row);
+        } else if at.y < self.len() {
+            let row = self.rows.get_mut(at.y).unwrap();
+            
+            row.insert(at.x, c);
+        }
+    }
+
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+
+        if at.y >= len {
+            return;
+        }
+
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+
+            row.delete(at.x);
+        }
+
+        let row = self.rows.get_mut(at.y).unwrap();
+
+        row.delete(at.x);
+    }
+
+    fn insert_newline(&mut self, at: &Position) {
+        let len = self.len();
+        if at.y > len {
+            return;
+        }
+
+        if at.y == len {
+            self.rows.push(Row::default());
+
+            return;
+        }
+
+        let new_row = self.rows.get_mut(at.y)
+            .unwrap()
+            .split(at.x);
+        
+        self.rows.insert(at.y + 1, new_row);
     }
 }
