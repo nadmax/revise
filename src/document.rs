58a80c6c712d5a6699d19c7da693184c53b46bbb
nadmax <n.maximilien1@gmail.com>
2023-08-06 14:24:33 +0200

    add proper document in order to take a file into account as an argument

---
+++ src/document.rs
@@ -0,0 +1,37 @@
+use crate::Row;
+
+use std::fs;
+use std::io::Error;
+
+#[derive(Default)]
+pub struct Document {
+    rows: Vec<Row>,
+}
+
+impl Document {
+    /// # Errors
+    /// 
+    /// Will return `Error` if it fails read filename
+    pub fn open(filename: &str) -> Result<Self, Error> {
+        let contents = fs::read_to_string(filename)?;
+        let mut rows = Vec::new();
+
+        for value in contents.lines() {
+            rows.push(Row::from(value));
+        }
+
+        Ok(Self {
+            rows
+        })
+    }
+
+    #[must_use]
+    pub fn row(&self, index: usize) -> Option<&Row> {
+        self.rows.get(index)
+    }
+
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
+    }
+}
