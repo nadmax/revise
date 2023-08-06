dd5b92df5b0e4336420e88a9289d6458c6bc4d5c
nadmax <n.maximilien1@gmail.com>
2023-08-06 17:30:25 +0200

    add characters insertion with deletion

--- src/document.rs
+++ src/document.rs
@@ -1,7 +1,9 @@
 use crate::Row;
+use crate::Position;
 
 use std::fs;
 use std::io::Error;
+use std::cmp::Ordering;
 
 #[derive(Default)]
 pub struct Document {
@@ -40,5 +42,78 @@ impl Document {
     #[must_use]
     pub fn len(&self) -> usize {
         self.rows.len()
+    }
+
+    /// # Panics
+    /// 
+    /// Will panic if there is no row to insert
+    pub fn insert(&mut self, at: &Position, c: char) {
+        let len = self.len();
+        
+        if c == '\n' {
+            self.insert_newline(at);
+
+            return;
+        }
+
+        match at.y.cmp(&len) {
+            Ordering::Equal => {
+                let mut row = Row::default();
+        
+                row.insert(0, c);
+                self.rows.push(row);
+            },
+            Ordering::Less => {
+                let row = self.rows.get_mut(at.y).unwrap();
+                
+                row.insert(at.x, c);
+            },
+            Ordering::Greater => (),
+        }
+    }
+
+    /// # Panics
+    /// 
+    /// Will panic if there is no row to delete
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+
+        if at.y >= len {
+            return;
+        }
+
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+
+            row.delete(at.x);
+        }
+
+        let row = self.rows.get_mut(at.y).unwrap();
+
+        row.delete(at.x);
+    }
+
+    fn insert_newline(&mut self, at: &Position) {
+        let len = self.len();
+        if at.y > len {
+            return;
+        }
+
+        if at.y == len {
+            self.rows.push(Row::default());
+
+            return;
+        }
+
+        let new_row = self.rows.get_mut(at.y)
+            .unwrap()
+            .split(at.x);
+        
+        self.rows.insert(at.y + 1, new_row);
     }
 }
--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
+#[derive(Default)]
 pub struct Row {
     string: String,
     len: usize,
@@ -49,6 +50,69 @@ impl Row {
     #[must_use]
     pub fn is_empty(&self) -> bool {
         self.len == 0
+    }
+
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len() {
+            self.string.push(c);
+        } else {
+            let mut result: String = self.string[..]
+                .graphemes(true)
+                .take(at)
+                .collect();
+            let remainder: String = self.string[..]
+                .graphemes(true)
+                .skip(at)
+                .collect();
+            
+            result.push(c);
+            result.push_str(&remainder);
+            self.string = result;
+        }
+        self.update_len();
+    }
+
+    pub fn delete(&mut self, at: usize){
+        if at >= self.len() {
+            return;
+        }
+
+        let mut result: String = self.string[..]
+            .graphemes(true)
+            .take(at)
+            .collect();
+        let remainder: String = self.string[..]
+            .graphemes(true)
+            .skip(at + 1)
+            .collect();
+    
+        result.push_str(&remainder);
+        self.string = result;
+        
+        self.update_len();
+    }
+
+    pub fn append(&mut self, new: &Self) {
+        self.string = format!("{}{}", self.string, new.string);
+
+        self.update_len();
+    }
+
+    #[must_use]
+    pub fn split(&mut self, at: usize) -> Self {
+        let beginning: String = self.string[..]
+            .graphemes(true)
+            .take(at)
+            .collect();
+        let remainder: String = self.string[..]
+            .graphemes(true)
+            .skip(at + 1)
+            .collect();
+        self.string = beginning;
+        self.update_len();
+
+
+        Self::from(&remainder[..])
     }
 
     fn update_len(&mut self) {
